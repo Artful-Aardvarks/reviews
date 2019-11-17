72aea46093a1dac976db44ddd6ac32cca5eb4357
Harry Shapiro <harrydshapiro96@gmail.com>
2019-11-16 19:42:30 -0500

    tweaked router

--- server/app.js
+++ server/app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const reviewsRouter = require("./routes.js");
+const path = require("path");
 
 const app = express();
 
 const jsonParser = bodyParser.json();
 app.use(jsonParser);
+
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.use("/reviews", reviewsRouter);
 
